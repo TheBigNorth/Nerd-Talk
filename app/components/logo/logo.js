43e0228fb7ca999dc63737c24d3e6a20d345668c
Gemma Black <gblack@googlemail.com>
2017-09-27 08:27:36 +0100

    Modules Separated Into Own Files & Logo Markup Started
    Logo Styling To Go With Markup Has Been Initiated

---
+++ app/components/logo/logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function() {
+    return (<h1 className="logo">
+        <span className="logo__circle"></span>
+        <span className="logo__nerd-talk">Nerd Talk</span>
+        <span className="logo__code-speak">Code Speak</span>
+    </h1>);
+}
