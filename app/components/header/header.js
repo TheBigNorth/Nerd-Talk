7116d4bb89da6f99cf2fdb401f1a20b9480447e7
Gemma Black <gblack@googlemail.com>
2017-10-02 07:52:29 +0100

    Logo Created With New Style Rules
    The project has more structure. Everything is currently its own component also.

---
+++ app/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../logo/logo.js';
+
+export default function(props) {
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <Logo></Logo>
+            </div>
+            <div className="header__hamburger">
+                
+            </div>
+        </div>
+    );
+}
