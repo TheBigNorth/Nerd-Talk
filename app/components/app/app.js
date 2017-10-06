eb471dbdd54afc6c0e3bf6a6a5ff2750810f9464
Gemma Black <gblack@googlemail.com>
2017-10-06 20:50:51 +0100

    Article Copy Created For Mobile

--- app/components/app/app.js
+++ app/components/app/app.js
@@ -15,7 +15,7 @@ export default class App extends Component {
                         <div className="section__inner padded">
                             <div className="post-intro">
                                 <h1 className="post-intro__title">
-                                    Nerd Talk launches a developer hoodie: The Story
+                                    Nerd Talk launches the passive aggressive developer hoodie
                                 </h1>
                                 <date className="post-intro__date">
                                     Monday 1 October 2017
@@ -26,17 +26,81 @@ export default class App extends Component {
                     <section className="section">
                         <div className="section__inner padded typography typography--article">
                             <img src="https://vangogh.teespring.com/v3/image/cb-x8vy8F1ntz6UthLhu4r_Ls3Y/480/560.jpg" />
-                            <p>Nice!</p>
-                            <p>You've taken time out from facebook 
-                                or whatever you've been doing. So I'll be brief and quick.
+                            <h3>Is it just a hoodie?</h3>
+                            <p>Yeah...</p>
+                            <p>Sorry.</p>
+                            <p>It's a normal hoodie. It doesn't have any new exciting fibers
+                                discovered in the mountains of Peru. It hasn't been handstitched
+                                using 1,000 year-old secrets passed from generation to generation.
+                                It wasn't dyed with organic dye from sea moluscs.
                             </p>
-                            <h2>Why a hoodie?</h2>
-                            <p>Most developers I know are mature, advanced and use the best practices. 
-                                The problems they have seem less and less about the code they write, 
-                                but the libraries, the documentation, the api, the build, authorisation 
-                                and working with everyone outside the dev team.
+                            <h3>Can we get a discount?</h3>
+                            <p>Of course! You can get 30% off when you join 
+                                Nerd Talk Weekly. Currently we have no plans to send a newsletter
+                                yet as we're just beginning, but we plan to in future.
                             </p>
-                            <p>If that's you, it's part of being a developer.</p>
+                            <p>
+                                <button>Get a discount?</button>
+                                <button>Just Buy</button>
+                            </p>
+                            <h3>So what's so special about it then?</h3>
+                            <p>Most of you guys write great code! 
+                                The problems they have however seem less about the code they write, 
+                                but working with clients and teams outside the dev bubble.
+                            </p>
+                            <p>Requirements change...last minute. What the client wanted, wasn't actually
+                                what the client wanted, and now months of work has to be restarted.
+                                A lot of the time, the client has no idea how much work is involved.
+                                They want to build another facebook, for little to no budget.
+                                They've told external clients that they can deliver a product within
+                                a certain amount of time without consulting you first 
+                                as to what will be needed to achieve it.
+                            </p>
+                            <p>It's not their fault. They just don't understand. So you have to spend
+                                time in meetings, explaining what can and cannot be achieved with
+                                100% custom code.
+                            </p>
+                            <p>I guess it's all part of the fun of being a developer.</p>
+                            <p>Hence: 99 problems but a bug ain't one.</p>
+                            <h3>Warning though</h3>
+                            <p>It occurred to me only after designing it. It is rather passive
+                                aggressive. So be discerning about when and where you wear it.
+                            </p>
+                            <h3>What about the hoodie itself?</h3>
+                            <p>The hoodies are custom made with the help of Teespring. They fulfill the
+                                order every 3 days - or after the campaign is finished.
+                            </p>
+                            <h3>So no wastage?</h3>
+                            <p>Nada. Teespring only print custom orders. That way, we're not adding
+                                unneeded rubbish to the earth. At least, not through unwanted hoodies.</p>
+                            <h3>Again. So, where’d the idea come from?</h3>
+                            <p>I won’t say who, but I saw steam. It sprayed out tne neck  
+                                of a fellow developer. They had just returned from a meeting. 
+                                They had 99 problems, but a bug wasn’t one. Cheesy I know.
+                            </p>
+                            <h3>Don’t like red?</h3>
+                            <p>No problem. It’s my favourite colour, but if you like 
+                                black or grey, we’ve got you covered.
+                            </p>
+                            <h3>Do I make the hoodies?</h3>
+                            <p>Lol. No! Look. I code. I don’t sew. 
+                                I don’t muck about with fabric. I just chose a company 
+                                I actually trust to deliver the designs with good quality 
+                                material and great customer service, in case you need to 
+                                send it back. In fact, Teespring have a great service, check them out. 
+                                If you like making slogans or are more arty, go ahead
+                                and create cool t-shirts and apparel with them too.
+                            </p>
+                            <h3>Yeah, what about returns?</h3>
+                            <p>Tee Spring will help you with returns. 
+                                You get a good amount of time to return it, with 30 days.
+                                And they have a customer service team which is handy.
+                                However, I can deal with issues. No problem.
+                            </p>
+                            <p>
+                                <button>Get a discount?</button>
+                                <button>Just Buy</button>
+                            </p>
                         </div>
                     </section>
                 </article>
